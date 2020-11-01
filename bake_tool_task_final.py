09c98ca529787f58cefe37416918e6b8976d2e5f
pshi <p.shi@tudelft.nl>
2020-11-01 11:41:13 +0100

    bake scripts update 1

--- bake_tool_task_final.py
+++ bake_tool_task_final.py
@@ -3,6 +3,7 @@ import bpy
 import math
 
 import os
+import shutil
 #clear the message in the system console
 os.system("cls")
 scene = bpy.context.scene
@@ -32,7 +33,12 @@ def clearAllJobs():
             bpy.ops.baketool.remove_job()
             print("remove one job")
             checkBakeJobsNum()
-
+def removesuffix(my_str, suffix):
+    # suffix='' should not call self[:-0].
+    if suffix and my_str.endswith(suffix):
+        return my_str[:-len(suffix)]
+    else:
+        return my_str[:]
 #get all the objs
 all_meshes_list=list(bpy.data.meshes)
 print(all_meshes_list)
@@ -68,20 +74,60 @@ all_jobs[0].name="AO"
 checkBakeJobsNum()
 filepath = bpy.data.filepath
 current_path = os.path.dirname(filepath)
+current_file_name=os.path.basename(filepath)
+current_file_name=removesuffix(current_file_name,".blend")
+bake_data_save_path=current_path+"/bake_"+current_file_name;
+if os.path.exists(bake_data_save_path):
+    shutil.rmtree(bake_data_save_path)
+os.mkdir(bake_data_save_path)
 
-os.mkdir("bake")
-#for i in range(len(mesh_list)):
-#    mesh=mesh_list[i];
-#     #mesh.set_select(state = True);
-#    print("current mesh:",mesh.name)
-#    current_obj=bpy.data.objects[mesh.name]
-#        
-#    bpy.context.view_layer.objects.active =current_obj
-#    bpy.ops.baketool.add_job()
-#    print(bpy.data.objects[mesh.name])
-# 
+#
+bake_tool_data=bpy.data.scenes["Scene"].BakeTool_Jobs;
+bake_tool_data.Jobs[0].job_settings.path=bake_data_save_path;
+
+print("*****************************************")
+print("Add meshes...")
+for i in range(len(mesh_list)):
+    mesh=mesh_list[i];
+   
+    print("current mesh:",mesh.name)
+    current_obj=bpy.data.objects[mesh.name]
+    current_obj.select_set(True)
+    bpy.ops.baketool.add_obj()
+    print("Select object:",current_obj.name)
+    #deselect the object, otherwise, it will be kept as selected status
+    current_obj.select_set(False)
+print("*****************************************")
+print("Check UVMap...")
+for i in range(len(mesh_list)):
+    current_mesh=mesh_list[i];
+   
+    print("current mesh:",current_mesh.name)
+    current_obj=bpy.data.objects[current_mesh.name]
+    current_obj.select_set(True)
+    #check if there is uv map for this object
+    all_uv_maps=list(current_mesh.uv_layers);
+    all_uv_maps_num=len(all_uv_maps);
+    print("uv_map_num:",all_uv_maps_num)
+    
+    
+    if(all_uv_maps_num==0):
+        bpy.ops.mesh.uv_texture_add()
+        #since there is only one uv map, so we can get the name
+        current_uv_layers=current_mesh.uv_layers;
+        print("currnet uv map name:",current_uv_layers[0].name)
+        #set the name of the uv map
+        #bpy.context.object.data.uv_layers["test_uv"].name = "ao_uv"
+
+    current_obj.select_set(False)
+    #
+    
+print("*****************************************")
+print("Set UV Map ...")
+
 
    
+bpy.context.scene.BakeTool_Jobs.Jobs[0].job_objs.coll 
         
  
 #check whether each obj has uv coordinates
